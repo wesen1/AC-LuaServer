01dfb7b1bc64036acb011bcef64564b707de9d5b
wesen <wesen1@users.noreply.github.com>
2020-12-27 05:13:03 +0100

    Improve ServerEventManager

--- tests/AC-LuaServer/Core/ServerEvent/TestServerEventManager.lua
+++ tests/AC-LuaServer/Core/ServerEvent/TestServerEventManager.lua
@@ -1,6 +1,6 @@
 ---
 -- @author wesen
--- @copyright 2019 wesen <wesen-ac@web.de>
+-- @copyright 2019-2020 wesen <wesen-ac@web.de>
 -- @release 0.1
 -- @license MIT
 --
@@ -28,7 +28,8 @@ TestServerEventManager.testClassPath = "AC-LuaServer.Core.ServerEvent.ServerEven
 -- @tfield table[] dependencyPaths
 --
 TestServerEventManager.dependencyPaths = {
-  { id = "LuaServerApi", path = "AC-LuaServer.Core.LuaServerApi", ["type"] = "table" }
+  { id = "LuaServerApi", path = "AC-LuaServer.Core.LuaServerApi", ["type"] = "table" },
+  { id = "Server", path = "AC-LuaServer.Core.Server" }
 }
 
 
@@ -37,18 +38,21 @@ TestServerEventManager.dependencyPaths = {
 --
 function TestServerEventManager:testCanHandleEventThatHasNoPredefinedHandler()
 
+  local LuaServerApiMock = self.dependencyMocks.LuaServerApi
   local ServerEventEmitter = self.testClass
   local eventEmitter = ServerEventEmitter()
 
   local eventCallback = self:getEventCallbackMock()
 
+  self:assertNil(LuaServerApiMock.onPlayerSayText)
   eventEmitter:on("onPlayerSayText", eventCallback)
+  self:assertNotNil(LuaServerApiMock.onPlayerSayText)
 
   eventCallback.call
                :should_be_called_with("/quit gravity")
                :and_will_return("PLUGIN_BLOCK")
                :when(function()
-                 local returnValue = self.dependencyMocks.LuaServerApi.onPlayerSayText("/quit gravity")
+                 local returnValue = LuaServerApiMock.onPlayerSayText("/quit gravity")
                  self:assertEquals("PLUGIN_BLOCK", returnValue)
                end)
 end
@@ -89,6 +93,99 @@ function TestServerEventManager:testCanHandleEventThatHasPredefinedHandler()
                                 self:assertNil(returnValue)
                                end
                          )
+
+end
+
+---
+-- Checks that exceptions of the event consumers are handled as expected.
+--
+function TestServerEventManager:testCanHandleExceptionsOfEventConsumers()
+
+  local Exception = require "AC-LuaServer.Core.Util.Exception.Exception"
+  local TemplateException = require "AC-LuaServer.Core.Util.Exception.TemplateException"
+  local LuaServerApiMock = self.dependencyMocks.LuaServerApi
+  local ServerEventEmitter = self.testClass
+  local eventEmitter = ServerEventEmitter()
+
+  local eventCallback = self:getEventCallbackMock()
+
+  eventEmitter:on("onPlayerSendMap", eventCallback)
+
+  -- TemplateException
+  local templateException = self:getMock(TemplateException, "TemplateExceptionMock")
+  templateException.is = self.mach.mock_method("is")
+  local serverMock = self:getMock("AC-LuaServer.Core.Server", "ServerMock")
+  local outputMock = self:getMock("AC-LuaServer.Core.Output.Output", "OutputMock")
+  eventCallback.call
+               :should_be_called_with(7, "gema_la_momie")
+               :and_will_raise_error(templateException)
+               :and_then(
+                 templateException.is
+                                  :should_be_called_with(TemplateException)
+                                  :and_will_return(true)
+               )
+               :and_then(
+                 self.dependencyMocks.Server.getInstance
+                                            :should_be_called()
+                                            :and_will_return(serverMock)
+                                            :and_then(
+                                              serverMock.getOutput
+                                                        :should_be_called()
+                                                        :and_will_return(outputMock)
+                                            )
+                                            :and_then(
+                                              outputMock.printException
+                                                        :should_be_called_with(templateException)
+                                            )
+               )
+               :when(
+                 function()
+                   local returnValue = LuaServerApiMock.onPlayerSendMap(7, "gema_la_momie")
+                   self:assertNil(returnValue)
+                 end
+               )
+
+  -- Exception
+  local normalException = self:getMock(Exception, "ExceptionMock")
+  normalException.is = self.mach.mock_method("is")
+  eventCallback.call
+               :should_be_called_with(3, "Gibbed-Gema10")
+               :and_will_raise_error(normalException)
+               :and_then(
+                 normalException.is
+                                :should_be_called_with(TemplateException)
+                                :and_will_return(false)
+               )
+               :and_then(
+                 normalException.is
+                                :should_be_called_with(Exception)
+                                :and_will_return(true)
+               )
+               :and_then(
+                 normalException.getMessage
+                                :should_be_called()
+                                :and_will_return("just a normal Exception but it is readable in the logs.")
+               )
+               :when(
+                 function()
+                   local success, result = pcall(LuaServerApiMock.onPlayerSendMap, 3, "Gibbed-Gema10")
+                   self:assertFalse(success)
+                   self:assertStrContains(result, "just a normal Exception but it is readable in the logs.")
+                 end
+               )
+
+  -- string
+  local errorMessage = "Something went wrong, don't tell the players!"
+  eventCallback.call
+               :should_be_called_with(13, "RoofTopGema")
+               :and_will_raise_error(errorMessage)
+               :when(
+                 function()
+                   local success, result = pcall(LuaServerApiMock.onPlayerSendMap, 13, "RoofTopGema")
+                   self:assertFalse(success)
+                   self:assertStrContains(result, "Something went wrong, don't tell the players!")
+                 end
+               )
 
 end
 
