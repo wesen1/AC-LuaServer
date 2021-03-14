1e478c4e5e46f1a2e9ac48934cc0ff5ce6b4558f
wesen <wesen1@users.noreply.github.com>
2021-03-14 19:30:53 +0100

    Add additionalParameters option to EventCallback

--- tests/AC-LuaServer/Core/Event/TestEventCallback.lua
+++ tests/AC-LuaServer/Core/Event/TestEventCallback.lua
@@ -80,6 +80,19 @@ function TestEventCallback:testCanBeCreatedFromObjectMethod()
   self:assertEquals(455, callback:call(4))
   self:assertEquals(430, callback:call(-21))
 
+
+  local anotherObjectMock = {
+    ["otherProperty"] = "hi",
+    ["generateString"] = function(_self, _appendString)
+      return _self.otherProperty .. _appendString
+    end
+  }
+
+  local otherCallback = EventCallback({anotherObjectMock, "generateString"})
+  self:assertEquals("hi I'm new", otherCallback:call(" I'm new"))
+  self:assertEquals("hi!! how to play?", otherCallback:call("!! how to play?"))
+  self:assertEquals("hi, i pro", otherCallback:call(", i pro"))
+
 end
 
 ---
@@ -90,9 +103,7 @@ function TestEventCallback:testCanBeCreatedFromObjectMethodWithNoParameters()
   local EventCallback = self.testClass
 
   local objectMock = {
-    ["myPropertyA"] = "perfect",
-    ["myPropertyB"] = "works",
-    ["myPropertyC"] = "fine"
+    ["myPropertyA"] = "perfect"
   }
 
   objectMock["methodWithNoParameters"] = function(...)
@@ -102,12 +113,50 @@ function TestEventCallback:testCanBeCreatedFromObjectMethodWithNoParameters()
     return objectMock.myPropertyA
   end
 
+  local callbackA = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithNoParameters"})
+  self:assertEquals("perfect", callbackA:call())
+
+  local callbackB = EventCallback({objectMock, "methodWithNoParameters"})
+  self:assertEquals("perfect", callbackB:call())
+
+end
+
+---
+-- Checks that a EventCallback can be created from a object's method that has a single parameter.
+--
+function TestEventCallback:testCanBeCreatedFromObjectMethodWithSingleParameter()
+
+  local EventCallback = self.testClass
+
+  local objectMock = {
+    ["myPropertyB"] = "works"
+  }
+
   objectMock["methodWithSingleParameter"] = function(...)
     local parameters = {...}
     self:assertEquals({ objectMock, 8 }, parameters)
 
     return objectMock.myPropertyB
   end
+
+  local callbackA = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithSingleParameter"})
+  self:assertEquals("works", callbackA:call(8))
+
+  local callbackB = EventCallback({objectMock, "methodWithSingleParameter"})
+  self:assertEquals("works", callbackB:call(8))
+
+end
+
+---
+-- Checks that a EventCallback can be created from a object's method that has multiple parameters.
+--
+function TestEventCallback:testCanBeCreatedFromObjectMethodWithMultipleParameters()
+
+  local EventCallback = self.testClass
+
+  local objectMock = {
+    ["myPropertyC"] = "fine"
+  }
 
   objectMock["methodWithMultipleParameters"] = function(...)
     local parameters = {...}
@@ -116,15 +165,229 @@ function TestEventCallback:testCanBeCreatedFromObjectMethodWithNoParameters()
     return objectMock.myPropertyC
   end
 
-  local callbackA = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithNoParameters"})
-  local callbackB = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithSingleParameter"})
-  local callbackC = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithMultipleParameters"})
+  local callbackA = EventCallback({["object"] = objectMock, ["methodName"] = "methodWithMultipleParameters"})
+  self:assertEquals("fine", callbackA:call("bla", "blub"))
 
-  self:assertEquals("perfect", callbackA:call())
-  self:assertEquals("works", callbackB:call(8))
-  self:assertEquals("fine", callbackC:call("bla", "blub"))
+  local callbackB = EventCallback({objectMock, "methodWithMultipleParameters"})
+  self:assertEquals("fine", callbackB:call("bla", "blub"))
 
 end
+
+---
+-- Checks that additional parameters can be added to a callback object method with no parameters.
+--
+function TestEventCallback:testCanAddAdditionalParametersToObjectMethodWithNoParameters()
+
+  local EventCallback = self.testClass
+
+  local objectMock = {}
+
+  -- Case A: Add single parameter at end position
+  objectMock["myPropertyA"] = "done"
+  objectMock["methodWithNoParameters"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "somevalue" }, parameters)
+
+    return objectMock.myPropertyA
+  end
+
+  local callbackA = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithNoParameters",
+      ["additionalParameters"] = { [1] = { "somevalue" } }
+  })
+  self:assertEquals("done", callbackA:call())
+
+  local callbackB = EventCallback({ objectMock, "methodWithNoParameters", { [1] = { "somevalue" } } })
+  self:assertEquals("done", callbackB:call())
+
+
+  -- Case B: Add multiple parameters at end position
+  objectMock["myPropertyB"] = "yes"
+  objectMock["methodWithNoParametersTwo"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "one", 2 }, parameters)
+
+    return objectMock.myPropertyB
+  end
+
+  local callbackC = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithNoParametersTwo",
+      ["additionalParameters"] = { [1] = { "one", 2 } }
+  })
+  self:assertEquals("yes", callbackC:call())
+
+  local callbackD = EventCallback({ objectMock, "methodWithNoParametersTwo", { [1] = { "one", 2 } } })
+  self:assertEquals("yes", callbackD:call())
+
+end
+
+---
+-- Checks that additional parameters can be added to a callback object method with parameters.
+--
+function TestEventCallback:testCanAddAdditionalParametersToObjectMethodWithParameters()
+
+  local EventCallback = self.testClass
+
+  local objectMock = {}
+
+  -- Case A: Single parameter at start
+  objectMock["myPropertyA"] = 1
+  objectMock["methodWithParametersOne"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, 1, "realfirst" }, parameters)
+
+    return objectMock.myPropertyA
+  end
+
+  local callbackA = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersOne",
+      ["additionalParameters"] = { [1] = { 1 } }
+  })
+  self:assertEquals(1, callbackA:call("realfirst"))
+
+  local callbackB = EventCallback({objectMock, "methodWithParametersOne", { [1] = { 1 } }})
+  self:assertEquals(1, callbackB:call("realfirst"))
+
+
+  -- Case B: Multiple parameter at start
+  objectMock["myPropertyB"] = 2
+  objectMock["methodWithParametersTwo"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, 2, 3, "iamarealargument" }, parameters)
+
+    return objectMock.myPropertyB
+  end
+
+  local callbackC = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersTwo",
+      ["additionalParameters"] = { [1] = { 2, 3 } }
+  })
+  self:assertEquals(2, callbackC:call("iamarealargument"))
+
+  local callbackD = EventCallback({objectMock, "methodWithParametersTwo", { [1] = { 2, 3 } }})
+  self:assertEquals(2, callbackD:call("iamarealargument"))
+
+
+  -- Case C: Single parameter in middle
+  objectMock["myPropertyC"] = 3
+  objectMock["methodWithParametersThree"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "yes", 99, "bla" }, parameters)
+
+    return objectMock.myPropertyC
+  end
+
+  local callbackE = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersThree",
+      ["additionalParameters"] = { [2] = { 99 } }
+  })
+  self:assertEquals(3, callbackE:call("yes", "bla"))
+
+  local callbackF = EventCallback({objectMock, "methodWithParametersThree", { [2] = { 99 } }})
+  self:assertEquals(3, callbackF:call("yes", "bla"))
+
+
+  -- Case D: Multiple parameter in middle
+  objectMock["myPropertyD"] = 4
+  objectMock["methodWithParametersFour"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "firstword", 1000, 9999, "secondword" }, parameters)
+
+    return objectMock.myPropertyD
+  end
+
+  local callbackG = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersFour",
+      ["additionalParameters"] = { [2] = { 1000, 9999 } }
+  })
+  self:assertEquals(4, callbackG:call("firstword", "secondword"))
+
+  local callbackH = EventCallback({objectMock, "methodWithParametersFour", { [2] = { 1000, 9999 } }})
+  self:assertEquals(4, callbackH:call("firstword", "secondword"))
+
+
+  -- Case E: Single parameter at end
+  objectMock["myPropertyE"] = 5
+  objectMock["methodWithParametersFive"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "a", "b", true }, parameters)
+
+    return objectMock.myPropertyE
+  end
+
+  local callbackI = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersFive",
+      ["additionalParameters"] = { [3] = { true } }
+  })
+  self:assertEquals(5, callbackI:call("a", "b"))
+
+  local callbackJ = EventCallback({objectMock, "methodWithParametersFive", { [3] = { true } }})
+  self:assertEquals(5, callbackJ:call("a", "b"))
+
+
+  -- Case F: Multiple parameters at end
+  objectMock["myPropertyF"] = 6
+  objectMock["methodWithParametersSix"] = function(...)
+    local parameters = {...}
+    self:assertEquals({ objectMock, "almostdone", "now", false, "notfalse" }, parameters)
+
+    return objectMock.myPropertyF
+  end
+
+  local callbackK = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersSix",
+      ["additionalParameters"] = { [3] = { false, "notfalse" } }
+  })
+  self:assertEquals(6, callbackK:call("almostdone", "now"))
+
+  local callbackL = EventCallback({objectMock, "methodWithParametersSix", { [3] = { false, "notfalse" } }})
+  self:assertEquals(6, callbackL:call("almostdone", "now"))
+
+
+  -- Case G: Parameters at multiple positions at once
+  objectMock["myPropertyG"] = 7
+  objectMock["methodWithParametersSeven"] = function(...)
+    local parameters = {...}
+    self:assertEquals(
+      { objectMock, "some", "before", "#1", "then", "more", "after", "#2", "and", "some", "at", "end" },
+      parameters
+    )
+
+    return objectMock.myPropertyG
+  end
+
+  local callbackM = EventCallback({
+      ["object"] = objectMock,
+      ["methodName"] = "methodWithParametersSeven",
+      ["additionalParameters"] = {
+        [1] = { "some", "before" },
+        [2] = { "then", "more", "after" },
+        [3] = { "and", "some", "at", "end" }
+      }
+  })
+  self:assertEquals(7, callbackM:call("#1", "#2"))
+
+  local callbackN = EventCallback({
+    objectMock,
+    "methodWithParametersSeven",
+    {
+      [1] = { "some", "before" },
+      [2] = { "then", "more", "after" },
+      [3] = { "and", "some", "at", "end" }
+    }
+  })
+  self:assertEquals(7, callbackN:call("#1", "#2"))
+
+end
+
 
 ---
 -- Checks that a invalid callback function config is handled as expected.
